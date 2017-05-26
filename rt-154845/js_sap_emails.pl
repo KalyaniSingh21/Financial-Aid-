4ff5518cefb916cac36dccabb62e84cdefc3988b
SPOT\singhk <singhk@potsdam.edu>
2017-05-26 11:48:00 -0400

    feature(fa) : add files to work on

---
+++ rt-154845/js_sap_emails.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use AIS::Common;
+use Date::Format;
+
+=head1 Script Name
+
+=head2 fin_xxxxx.pl
+
+=cut
+
+=head1 Documentation and Revision History
+
+! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+! REPORT:   js_sap_emails.sql
+!
+! SCRIPT:   js_sap_emails.pl
+!
+! DESC:     This script creates a CSV file of students who have a locked
+!           Eligibility Status record on ROASTAT for a given term.
+! 
+! INPUT:    
+!
+! OUTPUT:   
+!
+! KEYWORDS: 
+!
+! AUTHOR:  Janet van Weringh
+!
+! CREATED:  July 2011
+!
+! MODIFICATION HISTORY:
+!
+! 10-DEC-2012   MJT Converted to pdf_2_helios
+! 13-JAN-2015   JWB RT:140965
+!                   Rewrote the script for the updated request from the
+!                   Financial Aid department.
+! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! --------------------------------------------------------------------
+
+=cut
+
+Set_Env();
+
+my $data_dir = "$ENV{POT_DATA}/fin";
+my $scripts_dir = "$ENV{POT_SCRIPTS}/fin";
+my $script_name = 'js_sap_emails';
+my $log_file = "$data_dir/logs/$script_name.log";
+open( LOG_FILE, ">$log_file" );
+open( STDERR,   ">>$log_file") || warn "Not able to open $log_file.  $!";
+log_updt();
+
+my $term_code = $ARGV[0];
+
+if($term_code =~ /^js_/) {
+    ($term_code) = get_js_params($term_code);
+} 
+else {
+    printw("Enter the term code (e.g. 201602)");
+    $term_code = <STDIN>;
+}
+chomp($term_code);
+
+my $timestamp = time2str('_%Y-%m-%d-%H%M%S', time);
+my $sql_file = "$scripts_dir/$script_name.sql";
+my $csv_file = "$data_dir/$script_name$timestamp.csv";
+
+# The SQL file takes two parameters: Term Code and CSV File
+my $cmd_sql = "sqlplus / @ $sql_file " .
+              "$term_code $csv_file";
+exec_cmd($cmd_sql);
+if ( $? != 0 ) { croak($cmd_sql, $ENV{RUNUSER}, $0 ) && die "Croaked $!"; }
+log_updt();
+
+my $ftp_share = '/shares/finaid/reports/sap_emails';
+my $ftp_file = "$data_dir/$script_name.ftp";
+exec_cmd("unlink $ftp_file");
+open(FTP_FILE, ">$ftp_file");
+print FTP_FILE "cd $ftp_share \n";
+print FTP_FILE "put $csv_file \n";
+print FTP_FILE "quit \n";
+close(FTP_FILE);
+exec_cmd("ftp_2_helios $ftp_file");
+log_updt();
+
+close(LOG_FILE);
---
+++ rt-154845/js_sap_emails.sql
@@ -0,0 +1,91 @@
+/*
+ js_sap_emails.pl
+
+ AUTHOR:   Jeremy Bristol (JWB)
+
+ CREATED:  2016-01-13
+
+ DESCRIPTION:     RT:140965
+                  This script is used by js_sap_emails.pl to generate
+                  a CSV file of students who have a locked Eligibility
+                  Status record for the given term code.
+*/
+
+set serveroutput on
+set feedback off
+set pagesize 0
+set verify off
+set heading on
+set echo off
+set linesize 100
+set timing off
+
+-- Command Line parameters:
+-- &1 == Term Code
+-- &2 == CSV File Path
+define term_code = &1
+spool &2
+
+declare
+delim       constant char := ',';
+last_name   spriden.spriden_last_name%type;
+first_name  spriden.spriden_first_name%type;
+p_number    spriden.spriden_id%type;
+email       goremal.goremal_email_address%type;
+sap_code    rorsapr.rorsapr_sapr_code%type;
+
+cursor driving_cursor is
+
+    select
+        spriden_last_name,
+        spriden_first_name,
+        fp_spriden_id(rorsapr_pidm),
+        fp_get_email_addr(rorsapr_pidm, 'ON', 'A'),
+        rorsapr_sapr_code
+    from 
+        rorsapr, spriden
+    where 1=1
+        and spriden_pidm = rorsapr_pidm
+        and spriden_change_ind is null
+        and rorsapr_term_code = '&term_code'
+        and rorsapr_lock_ind = 'Y'
+    order by
+        spriden_last_name,
+        spriden_first_name
+;
+
+begin
+
+    dbms_output.enable(1000000);
+
+    open driving_cursor;
+
+        --Print the header:
+        dbms_output.put_line (
+            'Last Name' || delim ||
+            'First Name' || delim ||
+            'P Number' || delim ||
+            'Email' || delim ||
+            'SAP Code'
+        );
+
+    loop
+        fetch driving_cursor into
+            last_name, first_name, p_number, email, sap_code;
+        --Stop the loop when there is no more data:
+        exit when driving_cursor%notfound;
+
+        --Print the data:
+        dbms_output.put_line (
+            last_name || delim ||
+            first_name || delim ||
+            p_number || delim ||
+            email || delim ||
+            sap_code
+        );
+    end loop;
+
+end;
+/
+spool off
+exit
