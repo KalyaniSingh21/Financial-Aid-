e704eb5022f5271f64e2295ed0ead07ef951ad88
SPOT\singhk <singhk@potsdam.edu>
2017-05-31 08:33:37 -0400

    copied original file from the server

--- rt-154845/js_sap_emails.sql
+++ rt-154845/js_sap_emails.sql
@@ -34,33 +34,33 @@ first_name  spriden.spriden_first_name%type;
 p_number    spriden.spriden_id%type;
 email       goremal.goremal_email_address%type;
 sap_code    rorsapr.rorsapr_sapr_code%type;
-student_pidm spriden.spriden_pidm%type;
 
 cursor driving_cursor is
 
     select
         spriden_last_name,
         spriden_first_name,
-        fp_spriden_id(rorsapr_pidm),  --- change to spriden_pidm
-        fp_get_email_addr(rorsapr_pidm, 'ON', 'A'),
-        rorsapr_sapr_code,
-        spriden_pidm
+        fp_spriden_id(rorsapr_pidm),
+        fp_get_email_addr(rorsapr_pidm, 'ON', 'A', 'Y'),
+        rorsapr_sapr_code
     from
         rorsapr, spriden
-    where spriden_pidm = rorsapr_pidm
+    where 1=1
+        and spriden_pidm = rorsapr_pidm
         and spriden_change_ind is null
         and rorsapr_term_code = '&term_code'
         and rorsapr_lock_ind = 'Y'
     order by
         spriden_last_name,
-        spriden_first_name;
+        spriden_first_name
+;
 
 begin
 
     dbms_output.enable(1000000);
 
     open driving_cursor;
-    dbms_output.put_line ("In driving cursor");
+
         --Print the header:
         dbms_output.put_line (
             'Last Name' || delim ||
@@ -72,62 +72,9 @@ begin
 
     loop
         fetch driving_cursor into
-            last_name, first_name, p_number, email, sap_code, student_pidm;
+            last_name, first_name, p_number, email, sap_code;
         --Stop the loop when there is no more data:
-
-        dbms_output.put_line (student_pidm);
         exit when driving_cursor%notfound;
-
-      /*  insert into general.gurmail
-                  (gurmail_pidm,
-                   gurmail_system_ind,
-                   gurmail_term_code,
-                   gurmail_letr_code,
-                   gurmail_module_code,
-                   gurmail_admin_identifier,
-                   gurmail_matl_code_mod,
-                   gurmail_date_init,
-                   gurmail_date_printed,
-                   gurmail_user,
-                   gurmail_wait_days,
-                   gurmail_pub_gen,
-                   gurmail_init_code,
-                   gurmail_orig_ind,
-                   gurmail_activity_date,
-                   gurmail_aidy_code )
-         values   (student_pidm,
-                  'R',
-                  '999999',
-                  'FA_SAP_EMAIL',
-                   null,
-                   null,
-                   null,
-                   null,
-                   sysdate,
-                   user,
-                   null,
-                   null,
-                   null,
-                   null,
-                   sysdate,
-                   '&aid_year');
-
-          dbms_output.put_line ("Inserted Value");
-
-          UPDATE RRRAREQ y
-          SET y.rrrareq_trst_code = 'R'
-          WHERE y.rrrareq_aidy_code = '&aid_year'
-          and   y.rrrareq_treq_code = 'SAP'
-          and   y.rrrareq_pidm = student_pidm
-          and  exists (select 'RORSAPR_SAPR_CODE is U, W, R, P or B'
-                              from RORSAPR z
-                              where 1=1
-                                    and z.rorsapr_pidm = y.rrrareq_pidm
-                                    and z.rorsapr_term_code ='&term_code'
-                                    and z.rorsapr_sapr_code in ('U','W','R','P','B'));
-            dbms_output.put_line ("Updated Value");
-*/
-
 
         --Print the data:
         dbms_output.put_line (
@@ -140,6 +87,6 @@ begin
     end loop;
 
 end;
-
+/
 spool off
 exit
